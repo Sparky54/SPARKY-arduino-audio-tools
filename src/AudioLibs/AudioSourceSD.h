0f3df794fadb10239d9aedbaf40e8dc814934685
Sparky54 <100660496+Sparky54@users.noreply.github.com>
2023-07-05 02:33:42 -0600

    Add getFileSize()

--- src/AudioLibs/AudioSourceSD.h
+++ src/AudioLibs/AudioSourceSD.h
@@ -101,6 +101,8 @@ public:
   /// Provides the number of files (The max index is size()-1): WARNING this is very slow if you have a lot of files in many subdirectories
   long size() { return idx.size();}
 
+  long getFileSize() { return file.size(); }
+
 protected:
 #if defined(USE_SD_NO_NS) 
   SDDirect<SDClass, File> idx{SD};
